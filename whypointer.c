82d55664a3f75206879eda864c5dbd9214cbe763
Muhammed Answar C K <answarc764@gmail.com>
2024-09-19 10:06:49 +0530

    Assignment-16 is complete

---
+++ whypointer.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+
+int main(void) {
+    int numbers[10] = {1,2,3,4,5,6,7,8,9,0};
+    printf("%d\n", numbers); //We will get the memmory address where contents of the array is stored :)
+    return 0;
+}
